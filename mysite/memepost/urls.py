eeefac53cc76a598bdf4c8a049ced32b636aaf76
Rahmi Pruitt <rjpruitt16@gmail.com>
2017-05-11 03:39:55 +0000

    Finished displaying images
    I extended the site_base.html which is center piece for the pinax framework. The imagefield has built-in url attribute. I thought I needed detail view but I did not. The next commmit should be a form add to database through the front end.

--- mysite/memepost/urls.py
+++ mysite/memepost/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
                 url(r'^$', ListView.as_view(
                                     queryset=memepost.objects.all().order_by("-date")[:25],
                                     template_name="memepost/memepost.html")),
+                                    
+                url(r'^(?P<pk>\d+)$', DetailView.as_view(
+                                    model = memepost,
+                                    template_name="memepost/picture.html")),
+
             ]
