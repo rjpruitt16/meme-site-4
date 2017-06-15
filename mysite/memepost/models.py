65ca7c4413696c3282c6caa02f77026b29bd6e18
Rahmi Pruitt <rjpruitt16@gmail.com>
2017-06-15 02:17:44 +0000

    made test in memepost
    it is not working correctly. I also add pylint and refractored models/py

--- mysite/memepost/models.py
+++ mysite/memepost/models.py
@@ -1,17 +1,20 @@
+""" This file serves to be scheme for creating memes in my database. """
+
 from __future__ import unicode_literals
 from django.forms import ModelForm
 from django.db import models
 
 # Create your models here.
 class memepost(models.Model):
-    title = models.CharField(max_length = 140)
-    model_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/no-img.jpg')
+    title = models.CharField(max_length=140)
+    model_pic = models.ImageField(upload_to='pic_folder/', default='pic_folder/no-img.jpg')
     date = models.DateTimeField()
-    
+
     def __unicode__(self):
         return self.title
-        
+
 class memepostForm(ModelForm):
     class Meta:
         model = memepost
         fields = ["title", "model_pic", "date"]
+        
--- mysite/memepost/tests.py
+++ mysite/memepost/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+""" The files serves to test everything within the memepost app. """
 
-# Create your tests here.
+from django.test import TestCase
+from memepost.models import memepost, memepostForm
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class memepostTestCase(TestCase):
+    def setUp(self):
+        memepost.objects.create(title="foo", 
+            model_pic="/site_media/media/pic_folder/casual_headshot.jpg", 
+            date="2017-06-14")
+        memepost.objects.create(title="bar", 
+            date="2017-06-14")
+            
+    def test_memepost_enters_database(self):
+        foo = memepost.objects.get("foo")
+        bar = memepost.objects.get("bar")
+        self.assertEqual(foo.date, "6/15/17")
+        logger.debug("foo date is equal to %s", foo.date)
+        self.assertEqual(bar.model_pic, "no-img.jpg")
+        logger.debug("bar is equal to %s", bar.model_pic)
